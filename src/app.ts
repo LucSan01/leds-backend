1b1747b3e854fed47dc5bf83edcd997482202fe4
Lucky Osakwe <osakweluckychukwudi@gmail.com>
2025-08-17 01:26:50 +0100

    Debugging

--- src/app.ts
+++ src/app.ts
@@ -10,30 +10,35 @@ import cookieParser from "cookie-parser";
 
 export const app = express();
 
-
-
-// ✅ Load allowed origins from env
-const allowedOrigins = process.env.CORS_ORIGINS
-  ? process.env.CORS_ORIGINS.split(",").map((origin) => origin.trim())
-  : [];
-
-console.log("✅ Allowed Origins:", allowedOrigins);
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true); // allow Postman / server-to-server
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        console.warn("❌ CORS blocked:", origin);
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // allow cookies/authorization headers
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    origin: ["http://localhost:3000", "https://leds-gray.vercel.app"],
+    credentials: true,
   })
 );
+
+// ✅ Load allowed origins from env
+// const allowedOrigins = process.env.CORS_ORIGINS
+//   ? process.env.CORS_ORIGINS.split(",").map((origin) => origin.trim())
+//   : [];
+
+// console.log("✅ Allowed Origins:", allowedOrigins);
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true); // allow Postman / server-to-server
+//       if (allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         console.warn("❌ CORS blocked:", origin);
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true, // allow cookies/authorization headers
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
