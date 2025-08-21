5e4b1550d54ffad4f91c3a635e28c3163254f672
Lucky Osakwe <osakweluckychukwudi@gmail.com>
2025-08-21 23:29:37 +0100

    cors is a big issue

--- src/app.ts
+++ src/app.ts
@@ -6,29 +6,28 @@ config({
 import express, { Response, Request, NextFunction } from "express";
 import { errorHandlerFunc } from "./middleware/errorHandler";
 import cookieParser from "cookie-parser";
-import cors, { CorsOptions } from "cors";
 
 export const app = express();
 
-const allowedOrigins =
-  process.env.NODE_ENV === "production"
-    ? [process.env.FRONTEND_PROD!]
-    : [process.env.FRONTEND_DEV!];
+import cors from "cors";
 
-const corsOptions: CorsOptions = {
-  origin: allowedOrigins,
-  credentials: true,
-  // origin: (origin, callback) => {
-  //   if (!origin || allowedOrigins.includes(origin)) {
-  //     callback(null, true);
-  //   } else {
-  //     callback(new Error(`Not allowed by CORS`));
-  //   }
-  // },
-};
-app.use(cors(corsOptions));
-// Preflight for all routes
-app.options("*", cors(corsOptions));
+const allowedOrigins = ['https://leds-gray.vercel.app', 'http://localhost:3000'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      // Allow requests without origin or from allowedOrigins
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+// app.use(cors(corsOptions));
+// // Preflight for all routes
+// app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
